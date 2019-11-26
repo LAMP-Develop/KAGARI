fb74555bfcdbf379cd06b4d8cd307ee544a9632f
ShunyaSuzuki <anyushu2017@gmail.com>
2019-11-26 14:07:02 +0900

    update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('tel')->default('');
             $table->string('company')->default('');
-            $table->string('email')->unique();
+            $table->string('email', 191)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
