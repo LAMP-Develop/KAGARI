767a350b92286e8b357800a62cb7d7db7cf1b44c
Kota Ogino <okitsu090807@gmail.com>
2019-12-19 10:57:48 +0900

    半角英数字

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,7 @@ use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use App\Rules\AlphaNumHalf;
 
 class RegisterController extends Controller
 {
@@ -51,7 +52,7 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'password' => ['required', 'string', 'min:8', 'confirmed',new AlphaNumHalf],
             'company' => ['required','string'],
             'tel' => ['required', 'numeric', 'digits_between:8,12'],
         ]);
