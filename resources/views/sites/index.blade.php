69c1e3ee761bd1d08fce2bc348c71562b7ebfaf5
ShunyaSuzuki <anyushu2017@gmail.com>
2019-12-25 15:56:12 +0900

    fix

--- resources/views/sites/index.blade.php
+++ resources/views/sites/index.blade.php
@@ -1,125 +1,75 @@
 @section('content')
-<div class="message fixed-top m-4"></div>
 <section class="wrap">
-<div class="container">
-<table class="table text-muted">
-<thead>
-<tr>
-<th scope="col">サイトの名前</th>
-<th scope="col">サイトのジャンル</th>
-<th scope="col">ご契約プラン</th>
-<th scope="col">レポートのメール受信<i data-toggle="tooltip" data-original-title="月初にご登録メールアドレス宛に解析レポートPDFが送信されます。" class="ml-1 fas fa-question-circle"></i></th>
-<th></th>
-</th>
-</tr>
-</thead>
-<tbody>
-@if (count($add_sites) != 0)
-@foreach ($add_sites as $key => $site)
-<tr>
-<td>
-<div class="d-flex align-items-center">
-<figure class="m-0 line-height-1">
-<img class="d-inline-block align-middle" src="https://www.google.com/s2/favicons?domain={{ $site->url }}" width="16" height="16">
-</figure>
-<div class="ml-2">
-<span class="d-inline-block text-dark">{{ $site->site_name }}</span>
-<a class="ml-2 d-inline-block" href="{{ route('sites-edit', $site->id) }}"><i class="far fa-edit"></i></a>
-<span class="d-block"><small>{{ $site->url }}</small></span>
-</div>
-</div>
-</td>
-<td>{{ $categories[($site->category - 1)]->cat }}</td>
-<td>
-@if ($site->plan == null)
-<form class="" action="{{ route('plan') }}" method="post">
-@csrf
-<button type="submit" class="btn btn-link">プランを登録する</button>
-<input type="hidden" name="site-id" value="{{ $site->id }}">
-<input type="hidden" name="view-id" value="{{ $site->VIEW_ID }}">
-<input type="hidden" name="site-url" value="{{ $site->url }}">
-<input type="hidden" name="site-name" value="{{ $site->site_name }}">
-<input type="hidden" name="industries" value="{{ $site->industry }}">
-<input type="hidden" name="genre" value="{{ $site->category }}">
-</form>
-@else
-{{ $plans[($site->plan - 1)]->name }}
-@endif
-</td>
-<td class="text-center">
-<div class="custom-control custom-switch">
-@if($site->send_flag)
-<input id="checkbox-{{ $site->id }}" data-id="{{ $site->id }}" name="send_flag" type="checkbox" class="custom-control-input" value="0" checked>
-@else
-<input id="checkbox-{{ $site->id }}" data-id="{{ $site->id }}" name="send_flag" type="checkbox" class="custom-control-input" value="1">
-@endif
-<label class="custom-control-label" for="checkbox-{{ $site->id }}"></label>
-</div>
-</td>
-<td>
-<div class="d-flex align-items-center justify-content-end">
-@if ($site->plan == null)
-<a href="#" class="btn btn-sm btn-outline-primary mr-2 disabled">レポートを作成する</a>
-@else
-<a href="{{ route('ga-report', $site->id) }}" class="btn btn-sm btn-outline-primary mr-2">レポートを作成する</a>
-@endif
-@if ($site->plan % 2 != 0 || $site->plan == null)
-<a href="#" class="btn btn-sm btn-outline-secondary disabled">SEO分析する</a>
-@else
-<a href="{{ route('seo-report', $site->id) }}" class="btn btn-sm btn-outline-secondary">SEO分析する</a>
-@endif
-</div>
-</td>
-</tr>
-@endforeach
-@else
-<tr>
-<td colspan="3">まだサイトが登録されていません。</td>
-</tr>
-@endif
-</tbody>
-</table>
-@if (count($add_sites) == 0)
-<div class="mt-5 text-center">
-<a href="{{ route('addsite') }}" class="btn btn-primary">サイトを追加する</a>
-</div>
-@else
-<div class="mt-3">
-<a href="{{ route('addsite') }}" class="text-primary"><i class="fas fa-plus mr-1"></i>サイトを追加する</a>
-</div>
-@endif
-</div>
+  <div class="container">
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">
+        <div class="row">
+          <div class="col-4"><span class="font-weight-bold text-dark">サイトの名前</span></div>
+          <div class="col-3"><span class="font-weight-bold text-dark">サイトのジャンル</span></div>
+          <div class="col-3"><span class="font-weight-bold text-dark">ご契約プラン</span></div>
+        </div>
+      </li>
+      @if (count($add_sites) != 0)
+      @foreach ($add_sites as $key => $site)
+      <li class="list-group-item list-group-item-action" data-toggle="collapse" href="#collapse-{{ $key }}" role="button" aria-expanded="false" aria-controls="collapse-{{ $key }}">
+        <div class="row align-items-center">
+          <div class="col-4 d-flex align-items-center">
+            <figure class="m-0 line-height-1">
+              <img class="d-inline-block align-middle" src="https://www.google.com/s2/favicons?domain={{ $site->url }}" width="16" height="16">
+            </figure>
+            <div class="ml-2">
+              <span class="d-inline-block text-dark">{{ $site->site_name }}</span>
+              <span class="d-block"><small>{{ $site->url }}</small></span>
+            </div>
+          </div>
+          <div class="col-3">{{ $categories[($site->category - 1)]->cat }}</div>
+          <div class="col-3">
+            @if ($site->plan == null)
+            <form class="" action="{{ route('plan') }}" method="post">
+            @csrf
+              <button type="submit" class="btn btn-link p-0">プランを登録する</button>
+              <input type="hidden" name="site-id" value="{{ $site->id }}">
+              <input type="hidden" name="view-id" value="{{ $site->VIEW_ID }}">
+              <input type="hidden" name="site-url" value="{{ $site->url }}">
+              <input type="hidden" name="site-name" value="{{ $site->site_name }}">
+              <input type="hidden" name="industries" value="{{ $site->industry }}">
+              <input type="hidden" name="genre" value="{{ $site->category }}">
+            </form>
+            @else
+            {{ $plans[($site->plan - 1)]->name }}
+            @endif
+          </div>
+        </div>
+        <div class="collapse" id="collapse-{{ $key }}">
+          <hr>
+          <div class="d-flex justify-content-between align-items-center">
+            <div class="">
+              @if ($site->plan != null)
+              <a href="{{ route('ga-report', $site->id) }}" class="btn btn-sm btn-primary mr-2">レポートを作成する</a>
+              @endif
+              @if ($site->plan % 2 == 0 && $site->plan != null)
+              <a href="{{ route('seo-report', $site->id) }}" class="btn btn-sm btn-secondary">SEO分析する</a>
+              @endif
+            </div>
+            <div class="">
+              <a href="#" class="btn btn-sm btn-outline-secondary mr-2">プランの変更</a>
+              <a href="#" class="btn btn-sm btn-outline-secondary">サイト情報変更</a>
+            </div>
+          </div>
+        </div>
+      </li>
+      @endforeach
+      @endif
+    </ul>
+    @if (count($add_sites) == 0)
+    <div class="mt-5 text-center">
+      <a href="{{ route('addsite') }}" class="btn btn-primary">サイトを追加する</a>
+    </div>
+    @else
+    <div class="mt-3">
+      <a href="{{ route('addsite') }}" class="text-primary"><i class="fas fa-plus mr-1"></i>サイトを追加する</a>
+    </div>
+    @endif
+  </div>
 </section>
-
-<script>
-$(function() {
-  const csrf = $('meta[name="csrf-token"]').attr('content');
-  $.ajaxSetup({
-    headers: {
-      'X-CSRF-TOKEN': csrf
-    }
-  });
-  $('[name="send_flag"]').change(function() {
-    $.ajax({
-      url: '/send-flag',
-      type: 'post',
-      datatype: 'json',
-      data: {
-        id: $(this).attr('data-id'),
-        flag: $(this).val(),
-      }
-    }).done(function() {
-      $('.message').html('<div class="alert alert-success alert-dismissible fade show" role="alert">更新されました。<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
-      setTimeout(() => {
-        $('.message>.alert').alert('close');
-      }, 3000);
-    }).fail(function(jqXHR, textStatus, errorThrown) {
-      $('.message').html('<div class="alert alert-danger alert-dismissible fade show" role="alert">予期せぬエラーが発生しました。<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
-      setTimeout(() => {
-        $('.message>.alert').alert('close');
-      }, 3000);
-    });
-  });
-});
-</script>
 @endsection
