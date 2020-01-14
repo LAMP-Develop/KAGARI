a15a927664b3bff1e5b89af260b9346e763af76e
ShunyaSuzuki <anyushu2017@gmail.com>
2020-01-14 11:13:07 +0900

    add maintenance

---
+++ resources/views/error/503.blade.php
@@ -0,0 +1,13 @@
+@section('title', 'メンテナンス・・・')
+@extends('layouts.app')
+@include('layouts.head')
+@include('layouts.header')
+@include('layouts.footer')
+
+@section('content')
+<section class="wrap">
+<div class="container pt-5 text-center">
+<h2 class="text-center">現在メンテナンス中です・・・</h2>
+</div>
+</section>
+@endsection
