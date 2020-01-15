0b7bbf49b47fd8bf81bc87026b1ac1d780d430cd
ShunyaSuzuki <anyushu2017@gmail.com>
2020-01-15 14:15:13 +0900

    update

--- resources/views/form/changeplan-send.blade.php
+++ resources/views/form/changeplan-send.blade.php
@@ -7,8 +7,8 @@
 @section('content')
 <section class="wrap">
 <div class="container">
-<h2 class="text-center font-weight-bold mb-4">送信が完了しました。</h2>
-<p class="text-center mb-0">ご入力されましたメールアドレスにプラン変更申請の内容を<br>お送り致しましたのでご確認のほどよろしくお願い申し上げます。</p>
+<h2 class="text-center font-weight-bold mb-4">プラン変更申請を受け付けました。</h2>
+<p class="text-center mb-0">ご入力されましたメールアドレスにプラン変更申請の内容を送信しましたの合わせてご確認ください。</p>
 </div>
 </section>
 @endsection
