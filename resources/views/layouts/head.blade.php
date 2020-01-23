88184f26994af2f8f54cbfbb12c8b2081fe09d9a
ShunyaSuzuki <anyushu2017@gmail.com>
2020-01-23 13:53:09 +0900

    add favicon

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -3,6 +3,12 @@
 <meta name="robots" content="noindex">
 <meta name="viewport" content="width=1366,initial-scale=1">
 
+<link rel="shortcut icon" href="{{ asset('img/favicon.ico') }}">
+<link rel="icon" href="{{ asset('img/cropped-favicon-1-32x32.png') }}" sizes="32x32">
+<link rel="icon" href="{{ asset('img/cropped-favicon-1-192x192.png') }}" sizes="192x192">
+<link rel="apple-touch-icon-precomposed" href="{{ asset('img/cropped-favicon-1-180x180.png') }}">
+<meta name="msapplication-TileImage" content="{{ asset('img/cropped-favicon-1-270x270.png') }}">
+
 <!-- CSRF Token -->
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
