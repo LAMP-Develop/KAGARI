b4c668c165387606094850150e3af643ae05815c
Kota Ogino <okitsu090807@gmail.com>
2020-02-05 15:31:07 +0900

    SendMailCommand with send_flag

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -37,99 +37,104 @@ class TestController extends Controller
      */
     public function index()
     {
+        // $reigstered_sites = AddSites::where('send_flag',1)->get();
         // $now = date("Y-m-d H:i:s");
         // $now = new DateTime($now);
         // $today = date('d');
-        // $youbi = date('w');
+        // //月終わり
+        // $endDate  = new DateTime('last day of this month');
+        // if ($today == $endDate) {
+        //   $today = 31;
+        // }
+        // $date_w = date('w');
         // // if today is Monday
-        //   if ($youbi == 1) {
-        //     $sites = ReportSendDays::where('days',7)->get();
+        // if ($date_w == 1) {
+        //     $sites = ReportSendDays::where('days', 7)->get();
         //     foreach ($sites as $key => $site) {
-        //       $site_id = $site->site_id;
-        //       $action_url = URL::signedRoute('report-pdf.week', ['AddSites' => $site_id]);
-        //       $_site = AddSites::where('id', $site_id)->first();
-        //       $site_name = $_site->site_name;
-        //       $site_url = $_site->url;
-        //       try {
-        //           \Mail::to($_mail)->send(new ReportSendMailCrone($site_url, $site_name, $action_url));
-        //           \Mail::to($user_email)->send(new CustomerSendmail($site_url, $site_name, $action_url));
-        //       } catch (\Exception $e) {
-        //       }
-        //     }
-        //   }
-        //
-        // $sites = ReportSendDays::where('send_day',$today)->get();
-        //       foreach ($sites as $key => $site) {
-        //         $created_at = $site->created_at;
-        //         $created_at = new DateTime($created_at);
-        //         $diff = $now->diff($created_at);
-        //         $diff = $diff->m;
         //         $site_id = $site->site_id;
-        //         $site_days = $site->days;
-        //         if ($diff % 12 && $site_days == 360) {
-        //           $action_url = URL::signedRoute('report-pdf.year', ['AddSites' => $site_id]);
-        //           $site_mail = ReportSendMail::where('site_id',$site_id)->first();
-        //           $_mail = $site_mail->mailaddress;
-        //           $_site = AddSites::where('id', $site_id)->first();
-        //           $user_id = $_site->user_id;
-        //           $user = User::where('id',$user_id)->first();
-        //           $user_email = $user->email;
-        //           $site_name = $_site->site_name;
-        //           $site_url = $_site->url;
-        //           try {
-        //               \Mail::to($_mail)->send(new ReportSendMailCrone($site_url, $site_name, $action_url));
-        //               \Mail::to($user_email)->send(new CustomerSendmail($site_url, $site_name, $action_url));
-        //           } catch (\Exception $e) {
-        //           }
-        //         }elseif ($diff % 6 && $site_days == 180) {
-        //           $action_url = URL::signedRoute('report-pdf.six-month', ['AddSites' => $site_id]);
-        //           $site_mail = ReportSendMail::where('site_id',$site_id)->first();
-        //           $_mail = $site_mail->mailaddress;
-        //           $_site = AddSites::where('id', $site_id)->first();
-        //           $user_id = $_site->user_id;
-        //           $user = User::where('id',$user_id)->first();
-        //           $user_email = $user->email;
-        //           $site_name = $_site->site_name;
-        //           $site_url = $_site->url;
-        //           try {
-        //               \Mail::to($_mail)->send(new ReportSendMailCrone($site_url, $site_name, $action_url));
-        //               \Mail::to($user_email)->send(new CustomerSendmail($site_url, $site_name, $action_url));
-        //           } catch (\Exception $e) {
-        //           }
-        //         }elseif ($diff % 3 && $site_days == 90) {
-        //           $action_url = URL::signedRoute('report-pdf.three-month', ['AddSites' => $site_id]);
-        //           $site_mail = ReportSendMail::where('site_id',$site_id)->first();
-        //           $_mail = $site_mail->mailaddress;
-        //           $_site = AddSites::where('id', $site_id)->first();
-        //           $user_id = $_site->user_id;
-        //           $user = User::where('id',$user_id)->first();
-        //           $user_email = $user->email;
-        //           $site_name = $_site->site_name;
-        //           $site_url = $_site->url;
-        //           try {
-        //               \Mail::to($_mail)->send(new ReportSendMailCrone($site_url, $site_name, $action_url));
-        //               \Mail::to($user_email)->send(new CustomerSendmail($site_url, $site_name, $action_url));
-        //           } catch (\Exception $e) {
-        //           }
-        //         }elseif ($site_days == 30){
-        //           $action_url = URL::signedRoute('report-pdf.one-month', ['AddSites' => $site_id]);
-        //           $site_mail = ReportSendMail::where('site_id',$site_id)->first();
-        //           $_mail = $site_mail->mailaddress;
-        //           $_site = AddSites::where('id', $site_id)->first();
-        //           $user_id = $_site->user_id;
-        //           $user = User::where('id',$user_id)->first();
-        //           $user_email = $user->email;
-        //           $site_name = $_site->site_name;
-        //           $site_url = $_site->url;
-        //           try {
-        //               \Mail::to($_mail)->send(new ReportSendMailCrone($site_url, $site_name, $action_url));
-        //               \Mail::to($user_email)->send(new CustomerSendmail($site_url, $site_name, $action_url));
-        //               return true;
-        //           } catch (\Exception $e) {
-        //           }
-        //         }else {
-        //
-        //       }
+        //         $action_url = URL::signedRoute('report-pdf.week', ['AddSites' => $site_id]);
+        //         $_site = AddSites::where('id', $site_id)->first();
+        //         $site_name = $_site->site_name;
+        //         $site_url = $_site->url;
+        //         try {
+        //             \Mail::to($_mail)->send(new ReportSendMailCrone($site_url, $site_name, $action_url));
+        //             \Mail::to($user_email)->send(new CustomerSendmail($site_url, $site_name, $action_url));
+        //         } catch (\Exception $e) {
+        //         }
         //     }
+        // }
+        //
+        // $sites = ReportSendDays::where('send_day', $today)->get();
+        // foreach ($sites as $key => $site) {
+        //     $created_at = $site->created_at;
+        //     $created_at = new DateTime($created_at);
+        //     $diff = $now->diff($created_at);
+        //     $diff = $diff->m;
+        //     $site_id = $site->site_id;
+        //     $site_days = $site->days;
+        //     if ($diff % 12 && $site_days == 360) {
+        //         $action_url = URL::signedRoute('report-pdf.year', ['AddSites' => $site_id]);
+        //         $site_mail = ReportSendMail::where('site_id', $site_id)->first();
+        //         $_mail = $site_mail->mailaddress;
+        //         $_site = AddSites::where('id', $site_id)->first();
+        //         $user_id = $_site->user_id;
+        //         $user = User::where('id', $user_id)->first();
+        //         $user_email = $user->email;
+        //         $site_name = $_site->site_name;
+        //         $site_url = $_site->url;
+        //         try {
+        //             \Mail::to($_mail)->send(new ReportSendMailCrone($site_url, $site_name, $action_url));
+        //             \Mail::to($user_email)->send(new CustomerSendmail($site_url, $site_name, $action_url));
+        //         } catch (\Exception $e) {
+        //         }
+        //     } elseif ($diff % 6 && $site_days == 180) {
+        //         $action_url = URL::signedRoute('report-pdf.six-month', ['AddSites' => $site_id]);
+        //         $site_mail = ReportSendMail::where('site_id', $site_id)->first();
+        //         $_mail = $site_mail->mailaddress;
+        //         $_site = AddSites::where('id', $site_id)->first();
+        //         $user_id = $_site->user_id;
+        //         $user = User::where('id', $user_id)->first();
+        //         $user_email = $user->email;
+        //         $site_name = $_site->site_name;
+        //         $site_url = $_site->url;
+        //         try {
+        //             \Mail::to($_mail)->send(new ReportSendMailCrone($site_url, $site_name, $action_url));
+        //             \Mail::to($user_email)->send(new CustomerSendmail($site_url, $site_name, $action_url));
+        //         } catch (\Exception $e) {
+        //         }
+        //     } elseif ($diff % 3 && $site_days == 90) {
+        //         $action_url = URL::signedRoute('report-pdf.three-month', ['AddSites' => $site_id]);
+        //         $site_mail = ReportSendMail::where('site_id', $site_id)->first();
+        //         $_mail = $site_mail->mailaddress;
+        //         $_site = AddSites::where('id', $site_id)->first();
+        //         $user_id = $_site->user_id;
+        //         $user = User::where('id', $user_id)->first();
+        //         $user_email = $user->email;
+        //         $site_name = $_site->site_name;
+        //         $site_url = $_site->url;
+        //         try {
+        //             \Mail::to($_mail)->send(new ReportSendMailCrone($site_url, $site_name, $action_url));
+        //             \Mail::to($user_email)->send(new CustomerSendmail($site_url, $site_name, $action_url));
+        //         } catch (\Exception $e) {
+        //         }
+        //     } elseif ($site_days == 30) {
+        //         $action_url = URL::signedRoute('report-pdf.one-month', ['AddSites' => $site_id]);
+        //         $site_mail = ReportSendMail::where('site_id', $site_id)->first();
+        //         $_mail = $site_mail->mailaddress;
+        //         $_site = AddSites::where('id', $site_id)->first();
+        //         $user_id = $_site->user_id;
+        //         $user = User::where('id', $user_id)->first();
+        //         $user_email = $user->email;
+        //         $site_name = $_site->site_name;
+        //         $site_url = $_site->url;
+        //         try {
+        //             \Mail::to($_mail)->send(new ReportSendMailCrone($site_url, $site_name, $action_url));
+        //             \Mail::to($user_email)->send(new CustomerSendmail($site_url, $site_name, $action_url));
+        //         } catch (\Exception $e) {
+        //         }
+        //     } else {
+        //         return false;
+        //     }
+        // }
     }
 }
